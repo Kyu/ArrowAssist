cc2b6eb748b2d37f185d004182dbe0f7bcd89b54
Kyu <rocketword@gmail.com>
2021-01-04 04:51:29 -0600

    Changed to build for Forge 1.8.9-1722
    - Jars finally work
    - Not tested on other Forge versions

--- src/main/java/me/preciouso/ArrowAssist/ArrowAssist.java
+++ src/main/java/me/preciouso/ArrowAssist/ArrowAssist.java
@@ -8,7 +8,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 @Mod(modid = ArrowAssist.MODID, version = ArrowAssist.VERSION)
 public class ArrowAssist {
     public static final String MODID = "arrowassist";
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "0.0.2";
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
--- src/main/java/me/preciouso/ArrowAssist/ArrowListener.java
+++ src/main/java/me/preciouso/ArrowAssist/ArrowListener.java
@@ -5,7 +5,6 @@ import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
-import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
@@ -15,7 +14,6 @@ import java.util.Map;
 
 public class ArrowListener {
     public static HashMap<EntityArrow, Long> arrowTracker = new HashMap<>();
-
     public static long tickCount = 0;
 
     public boolean isArrowStuck(EntityArrow arrow) {
@@ -33,18 +31,15 @@ public class ArrowListener {
             for (Map.Entry<EntityArrow, Long> entry : arrowTracker.entrySet()) {
                 EntityArrow arr = entry.getKey();
                 if (arr != null && isArrowStuck(arr)) {
+                    // Tracking time since arrow shot
                     Long now = Minecraft.getSystemTime();
                     Long stuckTIme = entry.getValue();
 
                     if (stuckTIme == 0L) { // Store for 7 seconds
-                        World worldA = arr.getEntityWorld();
-                        if (!worldA.isRemote) {
+                        World worldIn = arr.getEntityWorld();
+                        if (!worldIn.isRemote) {
                             // Start tracking time
                             entry.setValue(now);
-
-                            // Start particles
-                            World worldIn = Minecraft.getMinecraft().theWorld;
-                            worldIn.spawnParticle(EnumParticleTypes.REDSTONE, arr.posX, arr.posY, arr.posZ, 0.0D, 10.0D, 0.0D, 1);
                         } else {
                             // TODO Works on multiplayer ?
                             toRemove.add(arr); // delete entry, world is not remote
@@ -53,8 +48,14 @@ public class ArrowListener {
                     } else if ((now - stuckTIme) >= 7 * 1000) {
                         toRemove.add(arr);
                     }  else {  // Else just spawn particle
-                        World worldIn = Minecraft.getMinecraft().theWorld;
-                        worldIn.spawnParticle(EnumParticleTypes.REDSTONE, arr.posX, arr.posY, arr.posZ, 0.0D, 10.0D, 0.0D, 1);
+                        // Start particles
+                        EnumParticleTypes rs = EnumParticleTypes.REDSTONE;
+                        double x = arr.posX;
+                        double y = arr.posY;
+                        double z = arr.posZ;
+
+                        // Sometimes null ?? idk
+                        Minecraft.getMinecraft().theWorld.spawnParticle(rs, x, y, z, 0.0D, 10.0D, 0.0D, 1);
                     }
                 } else if (arr == null) {
                     System.out.println("Null arrow!");
@@ -62,14 +63,14 @@ public class ArrowListener {
             }
             // Remove arrows from Hashmap
             arrowTracker.entrySet().removeIf(e -> toRemove.contains(e.getKey()));
-
         }
     }
 
-    @SubscribeEvent(priority=EventPriority.HIGHEST, receiveCanceled=true)
+    @SubscribeEvent
     public void onArrowShoot(EntityJoinWorldEvent event) {
         if (event.entity instanceof EntityArrow) {
             EntityArrow arrow = (EntityArrow) event.entity;
+
             if (arrow.shootingEntity != null) {  // Not world gen or world spawn
                 arrowTracker.put(arrow, 0L);
             }
